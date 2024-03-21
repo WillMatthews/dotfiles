cfe56e63aee585892b5883dc5d85398d795102b7
WillMatthews <will.a.matthews@me.com>
2024-03-21 18:14:50 +0000

    minor mods

--- .scripts/bootstrap.sh
+++ .scripts/bootstrap.sh
@@ -2,6 +2,9 @@
 
 # Bootstrap script for fresh Ubuntu installs with enhanced visual output
 
+## TODO
+# hostname setup? user setup?
+
 # Color and formatting variables
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -38,9 +41,9 @@ cp -r ${HOME}/dotfiles/. ${HOME}/
 # Package lists
 regolithPackages="regolith-desktop regolith-session-flashback regolith-look-lascaille"
 nonServePackages="manpages groff tty-clock ghci cabal-install make firefox thunderbird filezilla vlc ansiweather flameshot mumble freecad kicad gnuplot handbrake texlive-full ngspice nginx obs-studio openttd qrencode gimp inkscape steam suckless-tools valgrind units transmission wireshark glances ripgrep iotop iftop ufw fail2ban mc ncdu ranger python3-pip build-essential git-lfs xfce4-notifyd"
-servePackages="git net-tools neofetch htop btop lm-sensors plocate samba docker.io neovim zsh thefuck ffmpeg whois pandoc autossh rsync sysstat nethogs jq gnupg openssh-client openssh-server tmux gzip nmap screen speedometer speedtest-cli zip unzip unrar wget curl highlight"
+servePackages="git net-tools neofetch htop btop lm-sensors plocate samba docker.io neovim zsh thefuck ffmpeg whois pandoc autossh rsync sysstat nethogs jq gnupg openssh-client openssh-server tmux gzip nmap screen speedometer speedtest-cli zip unzip unrar wget curl highlight certbot"
 pipPackages="yt-dlp" # TODO make pip work
-cabalPackages="hakyll"
+cabalPackages="hakyll" # TODO make cabal work
 removePackages="regolith-rofication"
 
 # Snap packages for non-server setup
@@ -49,12 +52,18 @@ snapNonServe="0ad"
 # Function to install packages
 install_packages() {
     sudo apt install $1
+    sudo apt remove $removePackages
+}
+
+pip_install_packages() {
+    pip install $1
 }
 
 # Update and upgrade the system, then autoremove
 set -e
 print_green "Updating and upgrading the system..."
 sudo apt update && sudo apt upgrade && sudo apt autoremove
+
 
 # Interactive Menu for selecting installation type
 print_blue "Select Installation Type:"
@@ -66,13 +75,20 @@ case $installType in
     1)
         print_green "Installing Server Packages..."
         install_packages "${servePackages}"
+        pip_install_packages "${pipPackages}"
         ;;
     2)
         print_green "Installing Non-Server Packages..."
         install_packages "${servePackages} ${nonServePackages} ${regolithPackages}"
+        pip_install_packages "${pipPackages}"
         snap install ${snapNonServe}
 
-        # TODO: Bind `flameshot gui` to print screen
+        # Bind `flameshot gui` to print screen key
+        gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot '[]'
+        gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+        gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'flameshot'
+        gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'flameshot gui'
+        gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding 'Print'
         ;;
     *)
         print_red "Invalid choice. Exiting."
@@ -120,3 +136,50 @@ sudo apt autoremove
 # so I am leaving them here for now.
 # hugo, letsencrypt, clamav, cowsay, cmatrix, ftp??, virtualhub??
 
+
+## TODO:
+# mumble server & setup, create systemd service too.
+
+# wget https://github.com/mumble-voip/mumble/releases/download/1.3.4/murmur-static_x86-1.3.4.tar.bz2
+# sudo mkdir /usr/local/murmur
+# sudo cp -r ./murmur-static_x86-1.3.4/* /usr/local/murmur/
+# sudo cp ./murmur-static_x86-1.3.4/murmur.ini /etc/murmur.ini
+# sudo groupadd -r murmur
+# sudo useradd -r -g murmur -m -d /var/lib/murmur -s /sbin/nologin murmur
+# sudo mkdir /var/log/murmur
+# sudo chown murmur:murmur /var/log/murmur
+# sudo chmod 0770 /var/log/murmur
+# echo "/var/log/murmur/*log {
+#  su murmur murmur
+#  dateext
+#  rotate 4
+#  missingok
+#  notifempty
+#  sharedscripts
+#  delaycompress
+#  postrotate
+#  /bin/systemctl reload murmur.service > /dev/null 2>/dev/null || true
+# endscript
+# }" | sudo tee -a /etc/logrotate.d/murmur
+#
+# # edit /etc/murmur.ini
+#
+# echo "[Unit]
+# Description=Mumble Server (Murmur)
+# Requires=network-online.target
+# After=network-online.target mariadb.service time-sync.target
+#
+# [Service]
+# User=murmur
+# Type=forking
+# ExecStart=/usr/local/murmur/murmur.x86 -ini /etc/murmur.ini
+# PIDFile=/var/run/murmur/murmur.pid
+# ExecReload=/bin/kill -s HUP \$MAINPID
+#
+# [Install]
+# WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/murmur.service
+#
+# sudo ufw allow 64738/tcp
+# sudo ufw allow 64738/udp
+#
+# sudo systemctl start murmur.service
